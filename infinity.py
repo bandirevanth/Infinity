d42ad98d3a419ef5da6cd1a8fb57573ee4db06e1
Bandi Revanth <dhanush24082014@gmail.com>
2022-03-22 17:53:09 +0530

    Create infinity.py

---
+++ infinity.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+bgcolor('black')
+color('green')
+speed(11)
+right(45)
+
+for i in range (150):
+  circle(30)
+    if 7 < 1 < 62:
+      left(5)
+    if 80 < i < 133:
+       right(5)
+    if i < 80:
+        forward(10)
+    else:
+         forward(5)
